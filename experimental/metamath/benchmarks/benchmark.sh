017f011c6d5f44636a3c593d1c8c64a049c7af5d
Nil Geisweiller <ngeiswei@gmail.com>
2026-03-17 06:03:28 +0200

    Add CSV support

--- experimental/metamath/benchmarks/benchmark.sh
+++ experimental/metamath/benchmarks/benchmark.sh
@@ -9,6 +9,9 @@
 # Prefix directory where PeTTa and MORK are installed
 hypdir="/home/nilg/Work/TrueAGI"
 
+# CSV filename
+csv_fn="benchmark-hard.csv"
+
 #############
 # Functions #
 #############
@@ -46,7 +49,7 @@ get_max_ram() {
 # Get the theorem corresponding to its name
 get_theorem() {
     case "$1" in
-        idrefl)
+        impid)
             echo "(→ (→ 𝜑 𝜓) (→ 𝜑 𝜑))"
             ;;
         id)
@@ -77,30 +80,44 @@ get_theorem() {
 # Run the full suite of benchmarks to produce benchmarking data
 # comparing PeTTa and MM2.
 
+echo "chainer,engine,problem,depth,solutions,time,ram" > ${csv_fn}
+
 # 1. Run forward chaining to produce all candidates up to depth 4
 echo "*************** Full Forward Chaining ***************"
-for depth in {1..1}; do
+for depth in {1..4}; do
     echo "------------ depth $depth ------------"
     # MM2
     dst=pc-fc-depth_${depth}-mm2.stdout
     depth_peano="$(i2p $depth)"
     sed -i "s/(ne [()S Z]\+)/(ne $depth_peano)/g" pc-fc.mm2
     /usr/bin/time --verbose "${hypdir}/MORK/target/release/mork" run pc-fc.mm2 &> $dst
+    # Gather statistics
+    solutions=$(count_mm2_solutions "$depth_peano" $dst)
+    user_time=$(get_user_time $dst)
+    max_ram=$(get_max_ram $dst)
     echo "* MM2"
     echo "  - File: $dst"
-    echo "  - Solution count: $(count_mm2_solutions "$depth_peano" $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    echo "  - Solution count: ${solutions}"
+    echo "  - Time (in second): ${user_times}"
+    echo "  - Maximum RAM (in byte): ${max_ram}"
+    # Write to CSV file
+    echo "forward,mork,full,${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 
     # MeTTa
     dst=pc-fc-depth_${depth}-petta.stdout
     sed -i "s/(= (depth) [0-9]\+)/(= (depth) $depth)/g" pc-fc.metta
     /usr/bin/time --verbose "${hypdir}/PeTTa/run.sh" pc-fc.metta --silent &> $dst
+    # Gather statistics
+    solutions=$(count_petta_solutions $dst)
+    user_time=$(get_user_time $dst)
+    max_ram=$(get_max_ram $dst)
     echo "* PeTTa"
     echo "  - File: $dst"
-    echo "  - Solution count: $(count_petta_solutions $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    echo "  - Solution count: ${solutions}"
+    echo "  - Time (in second): ${user_time}"
+    echo "  - Maximum RAM (in byte): ${max_ram}"
+    # Write to CSV file
+    echo "forward,petta,full,${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 done
 
 # 2. Run backward chaining to produce all candidates up to depth 3
@@ -118,12 +135,17 @@ for depth in {1..2}; do
     # Remove query
     head -n -1 pc-bc.mm2 > pc-bc-tmp.mm2
     mv pc-bc-tmp.mm2 pc-bc.mm2
-    # Display statistics
+    # Gather statistics
+    solutions=$(count_mm2_solutions "\." $dst)
+    user_time=$(get_user_time $dst)
+    max_ram=$(get_max_ram $dst)
     echo "* MM2"
     echo "  - File: $dst"
-    echo "  - Solution count: $(count_mm2_solutions "\." $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    echo "  - Solution count: ${solutions}"
+    echo "  - Time (in second): ${user_time}"
+    echo "  - Maximum RAM (in byte): ${max_ram}"
+    # Write to CSV file
+    echo "backward,mork,full,${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 
     # MeTTa
     dst=pc-bc-depth_${depth}-petta.stdout
@@ -135,38 +157,48 @@ for depth in {1..2}; do
     # Remove query
     head -n -1 pc-bc.metta > pc-bc-tmp.metta
     mv pc-bc-tmp.metta pc-bc.metta
-    # Display statistics
+    # Gather statistics
+    solutions=$(count_petta_solutions $dst)
+    user_time=$(get_user_time $dst)
+    max_ram=$(get_max_ram $dst)
     echo "* PeTTa"
     echo "  - File: $dst"
-    echo "  - Solution count: $(count_petta_solutions $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    echo "  - Solution count: ${solutions}"
+    echo "  - Time (in second): ${user_time}"
+    echo "  - Maximum RAM (in byte): ${max_ram}"
+    # Write to CSV file
+    echo "backward,petta,full,${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 done
 
 # 3. Run backward chaining to produce all proofs of a given theorems
 #    of various solution densities
 echo "*************** Theorem Backward Chaining ***************"
-for theorem_id in idrefl id 2a1 pm2.43 imim2 jarr; do
-    depth=3
+for theorem_id in impid id 2a1 pm2.43 imim2 jarr; do
+    depth=4
     theorem="$(get_theorem $theorem_id)"
     echo "------------ Theorem: $theorem_id, $theorem ------------"
-    # MM2
-    dst=pc-bc-depth_${depth}-${theorem_id}-mm2.stdout
-    depth_peano="$(i2p $depth)"
-    query="(! (bc $depth_peano . (: \$x $theorem)))"
-    # Set query
-    echo "$query" >> pc-bc.mm2
-    # Run query
-    /usr/bin/time --verbose "${hypdir}/MORK/target/release/mork" run pc-bc.mm2 &> $dst
-    # Remove query
-    head -n -1 pc-bc.mm2 > pc-bc-tmp.mm2
-    mv pc-bc-tmp.mm2 pc-bc.mm2
-    # Display statistics
-    echo "* MM2"
-    echo "  - File: $dst"
-    echo "  - Solution count: $(count_mm2_solutions "\." $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    # # MM2
+    # dst=pc-bc-depth_${depth}-${theorem_id}-mm2.stdout
+    # depth_peano="$(i2p $depth)"
+    # query="(! (bc $depth_peano . (: \$x $theorem)))"
+    # # Set query
+    # echo "$query" >> pc-bc.mm2
+    # # Run query
+    # /usr/bin/time --verbose "${hypdir}/MORK/target/release/mork" run pc-bc.mm2 &> $dst
+    # # Remove query
+    # head -n -1 pc-bc.mm2 > pc-bc-tmp.mm2
+    # mv pc-bc-tmp.mm2 pc-bc.mm2
+    # # Gather statistics
+    # solutions=$(count_mm2_solutions "\." $dst)
+    # user_time=$(get_user_time $dst)
+    # max_ram=$(get_max_ram $dst)
+    # echo "* MM2"
+    # echo "  - File: $dst"
+    # echo "  - Solution count: $solutions"
+    # echo "  - Time (in second): $user_time"
+    # echo "  - Maximum RAM (in byte): $max_ram"
+    # # Write to CSV file
+    # echo "backward,mork,${theorem_id},${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 
     # MeTTa
     dst=pc-bc-depth_${depth}-${theorem_id}-petta.stdout
@@ -178,11 +210,15 @@ for theorem_id in idrefl id 2a1 pm2.43 imim2 jarr; do
     # Remove query
     head -n -1 pc-bc.metta > pc-bc-tmp.metta
     mv pc-bc-tmp.metta pc-bc.metta
-    # Display statistics
+    # Gather statistics
+    solutions=$(count_petta_solutions $dst)
+    user_time=$(get_user_time $dst)
+    max_ram=$(get_max_ram $dst)
     echo "* PeTTa"
     echo "  - File: $dst"
-    echo "  - Solution count: $(count_petta_solutions $dst)"
-    echo "  - Time (in second): $(get_user_time $dst)"
-    echo "  - Maximum RAM (in byte): $(get_max_ram $dst)"
+    echo "  - Solution count: ${solutions}"
+    echo "  - Time (in second): ${user_time}"
+    echo "  - Maximum RAM (in byte): ${max_ram}"
+    # Write to CSV file
+    echo "backward,petta,${theorem_id},${depth},${solutions},${user_time},${max_ram}" >> ${csv_fn}
 done
-
